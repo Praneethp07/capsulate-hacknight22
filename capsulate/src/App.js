c3d0e8caad6002bd5f2114a563fe12c5311999e1
praneeth <praneethp.cs20@sahyadri.edu.in>
2022-10-30 19:42:15 +0530

    first commit

---
+++ capsulate/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Headder from './components/headder/Headder';
+import Footer from './components/footer/Footer';
+
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Headder />
+      <Body />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
+
+
