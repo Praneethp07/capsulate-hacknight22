3e2795754103152e8f6410a31f4828398ebd62ef
praneeth <praneethp.cs20@sahyadri.edu.in>
2022-11-03 23:56:15 +0530

    Updates

---
+++ precache-manifest.b6e4ab6789be1b5d1fe655a11719d13d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "84278b4fd558a620c6ca",
+    "url": "/capsulate-hacknight22/static/css/main.4ff26350.chunk.css"
+  },
+  {
+    "revision": "84278b4fd558a620c6ca",
+    "url": "/capsulate-hacknight22/static/js/main.84278b4f.chunk.js"
+  },
+  {
+    "revision": "e36667d0eb2f7ff0b780",
+    "url": "/capsulate-hacknight22/static/js/1.e36667d0.chunk.js"
+  },
+  {
+    "revision": "8129ae88d7366a2c5008",
+    "url": "/capsulate-hacknight22/static/js/runtime~main.8129ae88.js"
+  },
+  {
+    "revision": "ba754cf2ac8d308dae45a1ab30f463cc",
+    "url": "/capsulate-hacknight22/static/media/image1.ba754cf2.jpg"
+  },
+  {
+    "revision": "0abd6c3df84a376d1902f58c7e0397ac",
+    "url": "/capsulate-hacknight22/static/media/image2.0abd6c3d.jpg"
+  },
+  {
+    "revision": "96d35dd239da4d18c34b30f41d5403e1",
+    "url": "/capsulate-hacknight22/static/media/image3.96d35dd2.jpg"
+  },
+  {
+    "revision": "52ce274195427cc41c701e42799c2f90",
+    "url": "/capsulate-hacknight22/static/media/image4.52ce2741.jpg"
+  },
+  {
+    "revision": "5723a4ee533c9dbc749245dcbf4bf47f",
+    "url": "/capsulate-hacknight22/static/media/image5.5723a4ee.jpg"
+  },
+  {
+    "revision": "077fbe99ee4c486760f2d9e50cdfd26d",
+    "url": "/capsulate-hacknight22/static/media/image6.077fbe99.jpg"
+  },
+  {
+    "revision": "cf068e147873b8085333774d16d15378",
+    "url": "/capsulate-hacknight22/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/capsulate-hacknight22/precache-manifest.b6e4ab6789be1b5d1fe655a11719d13d.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/capsulate-hacknight22/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
